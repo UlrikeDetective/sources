d1f9c6b3f46ab088ce9a43d7911ef92a8e63913f
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-02 21:58:17 +0200

    Update matplotlib_barchart_label.py

--- example_plots/Python/matplotlib_barchart_label.py
+++ example_plots/Python/matplotlib_barchart_label.py
@@ -62,3 +62,13 @@ ax.bar_label(hbars, labels=[f'±{e:.2f}' for e in error],
 ax.set_xlim(right=16)
 
 plt.show()
+
+# Bar labels using {}-style format string
+
+fruit_names = ['Coffee', 'Salted Caramel', 'Pistachio']
+fruit_counts = [4000, 2000, 7000]
+
+fig, ax = plt.subplots()
+bar_container = ax.bar(fruit_names, fruit_counts)
+ax.set(ylabel='pints sold', title='Gelato sales by flavor', ylim=(0, 8000))
+ax.bar_label(bar_container, fmt='{:,.0f}')
