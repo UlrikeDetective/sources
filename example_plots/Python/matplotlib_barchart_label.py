11d378b36fa99ba731c0d341adbac28c1331b6d7
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-02 21:55:49 +0200

    Update matplotlib_barchart_label.py

--- example_plots/Python/matplotlib_barchart_label.py
+++ example_plots/Python/matplotlib_barchart_label.py
@@ -50,7 +50,7 @@ plt.show()
 
 fig, ax = plt.subplots()
 
-hbars = ax.barh(y_pos, performance, xerr=error, align='center')
+hbars = ax.barh(y_pos, performance, xerr=error, align='center', color="green")
 ax.set_yticks(y_pos, labels=people)
 ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel('Performance')
@@ -58,7 +58,7 @@ ax.set_title('How fast do you want to go today?')
 
 # Label with given captions, custom padding and annotate options
 ax.bar_label(hbars, labels=[f'±{e:.2f}' for e in error],
-             padding=8, color='b', fontsize=14)
+             padding=6, color='slategrey', fontsize=14)
 ax.set_xlim(right=16)
 
 plt.show()
