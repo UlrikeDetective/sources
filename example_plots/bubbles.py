5b633d43aad298731e66ba0f42f45f13d8e22c45
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-02 15:56:42 +0200

    Create bubbles.py

---
+++ example_plots/bubbles.py
@@ -0,0 +1,25 @@
+import matplotlib as plt
+
+#ADD YOUR DATA HERE
+browser_market_share = {
+    'browsers': ['firefox', 'chrome', 'safari', 'edge', 'ie', 'opera'],
+    'market_share': [8.61, 69.55, 8.36, 4.12, 2.76, 2.43],
+    'color': ['#5A69AF', '#579E65', '#F9C784', '#FC944A', '#F24C00', '#00B825']
+}
+#STEP 3
+bubble_chart = BubbleChart(area=browser_market_share['market_share'],
+                           bubble_spacing=0.1)
+#STEP 4
+bubble_chart.collapse()
+
+#STEP 5
+
+fig, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
+bubble_chart.plot(
+    ax, browser_market_share['browsers'], browser_market_share['color'])
+ax.axis("off")
+ax.relim()
+ax.autoscale_view()
+ax.set_title('Browser market share')
+
+plt.show()
